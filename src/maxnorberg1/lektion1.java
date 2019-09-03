f6bccafb22ae0d11894ebebd8a1602d7249f93dc
maxnorberg1 <max.norberg@elev.ga.ntig.se>
2019-09-03 11:58:10 +0200

    Skapat lektion1

---
+++ src/maxnorberg1/lektion1.java
@@ -0,0 +1,11 @@
+package maxnorberg1;
+
+public class lektion1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("svante e bae");
+	}
+
+}
