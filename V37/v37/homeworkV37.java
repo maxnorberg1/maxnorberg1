b1f013e44bba6331772a2c26d00fd8583b124a5e
maxnorberg1 <max.norberg@elev.ga.ntig.se>
2019-09-15 17:18:23 +0200

    Update homeworkV37.java

--- V37/v37/homeworkV37.java
+++ V37/v37/homeworkV37.java
@@ -8,14 +8,24 @@ public class homeworkV37 {
 		
 		Scanner input = new Scanner(System.in);
 		
-		System.out.println("Vad heter du? ");
-
+		
+		System.out.println("Vad heter du?");
 		String namn1 = input.nextLine();
 		
-		
-		System.out.println("Hur gammal är du? ");
-		
+		System.out.println("Hur gammal är du?");
 		String age = input.nextLine();
+		
+		System.out.println("Vad är din adress?");
+		String adress = input.nextLine();
+		
+		System.out.println("Vad är ditt postnummer?");
+		String postnr = input.nextLine();
+		
+		System.out.println("Vilken stad bor du i?");
+		String stad = input.nextLine();
+		
+		System.out.println("Vad är ditt telefonnummer?");
+		String telenr = input.nextLine();
 		
 	}
 
