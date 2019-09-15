700450d8acf3519f1264167f6a1488a0fec32846
maxnorberg1 <max.norberg@elev.ga.ntig.se>
2019-09-15 17:02:48 +0200

    Update homeworkV37.java

--- V37/v37/homeworkV37.java
+++ V37/v37/homeworkV37.java
@@ -1,9 +1,14 @@
 package v37;
 
+import java.util.Scanner;
+
 public class homeworkV37 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		
+		
 
 	}
 
