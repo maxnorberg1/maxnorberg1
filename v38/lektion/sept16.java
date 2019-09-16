2958bb7c16d57a11aaaf7c7b729841a0a4c1f7ae
maxnorberg1 <max.norberg@elev.ga.ntig.se>
2019-09-16 13:21:15 +0200

    Create sept16.java

---
+++ v38/lektion/sept16.java
@@ -0,0 +1,45 @@
+package lektion;
+
+import java.util.Scanner;
+
+public class sept16 {
+
+	public static void main(String[] args) {
+
+		// villkor
+
+		int age = 0; // initierat variabel
+		int again = 1;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		do {
+			System.out.println("\tVill du testa igen? Skriv (1) om ja, (2) om nej");
+			again = 
+		}
+		while (!(again == 1)); 
+		
+
+		Scanner input = new Scanner(System.in); // gör det möjligt att få input från konsolen
+
+		if (again) {
+
+			System.out.println("\tFÅR DU KÖRA BIL QUIZ" + "\n\tHUR MÅNGA ÅR HAR DU FYLLT???!");
+			age = input.nextInt();
+
+			if (age >= 18 && age < 85) {
+
+				System.out.println("\twihoooo plattan i mattan! Grattis din lilla maddafakker!");
+			} else if (age > 14 && age < 18) {
+				System.out.println("\tLivet är roligare över 45 km/h :0");
+			} else if (age > 85) {
+				System.out.println("\tDu är en LEVANDE TRAFIKFARA");
+			} else {
+				System.out.println("\tpffft!! Ta det lugnt och sätt dig i din bobbycar....");
+			}
+			while (again != age); 
+			
+			
+		}
+	}  
+}
