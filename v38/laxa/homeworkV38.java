a7b4cb114dd7fb3335f0cddef8592ff22b9a10d3
maxnorberg1 <max.norberg@elev.ga.ntig.se>
2019-09-22 18:41:07 +0200

    Update homeworkV38.java

--- v38/laxa/homeworkV38.java
+++ v38/laxa/homeworkV38.java
@@ -17,39 +17,43 @@ public class homeworkV38 {
 			
 			int highest = 1;
 			int lowest = 1;
+			double summa = 1;
+			double average = 1;
 			
-			if (n1 < n2 && n1 < n3 && n1 < n4 && n1 < n5) {
+			if (n1 <= n2 && n1 <= n3 && n1 <= n4 && n1 <= n5) {
 				lowest = n1;
 			}
-			else if (n2 < n1 && n2 < n3 && n2 < n4 && n2 < n5) { 
+			else if (n2 <= n1 && n2 <= n3 && n2 <= n4 && n2 <= n5) { 
 				lowest = n2;
 			}
-			else if (n3 < n1 && n3 < n2 && n3 < n4 && n3 < n5) {
+			else if (n3 <= n1 && n3 <= n2 && n3 <= n4 && n3 <= n5) {
 				lowest = n3;
 			}
-			else if (n4 < n1 && n4 < n2 && n4 < n3 && n4 < n5) {
+			else if (n4 <= n1 && n4 <= n2 && n4 <= n3 && n4 <= n5) {
 				lowest = n4 ;
 			}
-			else if (n5 < n1 && n5 < n2 && n5 < n3 && n5 < n4) {
+			else if (n5 <= n1 && n5 <= n2 && n5 <= n3 && n5 <= n4) {
 				lowest = n5;
 			}
 			
-			if (n1 > n2 && n1 > n3 && n1 > n4 && n1 > n5) {
+			if (n1 >= n2 && n1 >= n3 && n1 >= n4 && n1 >= n5) {
 				highest = n1;
 			}
-			else if (n2 > n1 && n2 > n3 && n2 > n4 && n2 > n5) {
+			else if (n2 >= n1 && n2 >= n3 && n2 >= n4 && n2 >= n5) {
 				highest = n2;
 			}
-			else if (n3 > n1 && n3 > n2 && n3 > n4 && n3 > n5) {
+			else if (n3 >= n1 && n3 >= n2 && n3 >= n4 && n3 >= n5) {
 				highest = n3;
 			}
-			else if (n4 > n1 && n4 > n2 && n4 > n3 && n4 > n5) {
+			else if (n4 >= n1 && n4 >= n2 && n4 >= n3 && n4 >= n5) {
 				highest = n4;
 			}
-			else if (n5 > n1 && n5 > n2 && n5 > n3 && n5 > n4) {
+			else if (n5 >= n1 && n5 >= n2 && n5 >= n3 && n5 >= n4) {
 				highest = n5;
 			} 
-			System.out.println("\n\tDet högsta talet är: " + highest + "\n\tDet lägsta talet är: " + lowest);
+			summa = n1 + n2 + n3 + n4 + n5;
+			average = summa/5;
+			System.out.println("\n\tDet högsta talet är: " + highest + "\n\tDet lägsta talet är: " + lowest + "\n\tMedelvärdet är: " + average);
 			
 	} 
 
