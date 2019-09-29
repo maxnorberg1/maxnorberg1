69d07d1ffd5dba93cb15c0d6e5fe44737a0f7f86
maxnorberg1 <max.norberg@elev.ga.ntig.se>
2019-09-29 18:32:15 +0200

    Create homeworkR2.java

---
+++ v39/laxa/homeworkR2.java
@@ -0,0 +1,20 @@
+
+
+import java.util.Scanner;
+
+public class homeworkR2 {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner input = new Scanner(System.in);
+		
+		
+		int r1 = input.nextInt();
+		int s = input.nextInt();
+		
+		System.out.println((s- (r1-s)));
+
+	}
+
+}
