ae81ce65dcf3f2e704a518815f29ecb435a98b35
maxnorberg1 <max.norberg@elev.ga.ntig.se>
2019-09-30 09:51:37 +0200

    Create laxa_time_loop.java

---
+++ v39/laxa/laxa_time_loop.java
@@ -0,0 +1,19 @@
+package laxa;
+
+import java.util.Scanner;
+
+public class laxa_time_loop {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int n = input.nextInt();
+		
+		for (int i = 1; i <= n; i++) {
+			System.out.println(i + " Abracadabra");
+		}
+
+	}
+
+}
