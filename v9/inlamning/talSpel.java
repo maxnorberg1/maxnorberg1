7618793ba87533508561e36905c9f4d6252b032f
maxnorberg1 <max.norberg@elev.ga.ntig.se>
2020-03-08 20:43:36 +0100

    Update talSpel.java

--- v9/inlamning/talSpel.java
+++ v9/inlamning/talSpel.java
@@ -11,29 +11,46 @@ public class talSpel {
 	}
 	
 	public static void run() {
-		int repeat = 1;
+		boolean repeat = false;
 		
 	
-		do {
+		while (!repeat) {
 			Scanner in = new Scanner(System.in);
 			int firstChoice;
 			int again;
 			
+			boolean success = false;
+			
+			while (!success) {
+				
+				
+				try {
+					System.out.println("Welcome to my game! Please choose one of the alternatives below\n");
+					System.out.println("(1) Start Game");
+					System.out.println("(2) Game Instructions");
+					System.out.println("(3) Credit Screen");
+					System.out.println("(4) Exit Game");
+					System.out.println();
+					
+					firstChoice = in.nextInt();
+				
+
+				success = true;
+				
+				if (firstChoice < 1 || firstChoice > 4) {
+					System.out.println("** You did not enter one of the given numbers. Try again. **");
+					success = false;
+				}
+				
+				
 			
 			
-			System.out.println("Welcome to my game! Please choose one of the alternatives below\n");
-			System.out.println("(1) Start Game");
-			System.out.println("(2) Game Instructions");
-			System.out.println("(3) Credit Screen");
-			System.out.println("(4) Exit Game");
-			System.out.println();
 			
-			firstChoice = in.nextInt();
 			
 			switch (firstChoice) {
 			case 1: 
 				
-				repeat = 0;
+				
 				game();
 				
 				break;
@@ -48,10 +65,7 @@ public class talSpel {
 				System.out.println("(3) Now the fun begins! Guess away!");
 				System.out.println("(4) You will be notified if your guess is too high, too low, or just right! Try to guess the right number before you run out of attempts.");
 				System.out.println("(5) After playing, you will be asked if you want to play again.\n");
-				System.out.println("-- Press '0' to go back to main menu --");
-				if (in.nextInt() == 0) {
-					return;
-				}
+				pressZero();
 				break;
 				
 			case 3: 
@@ -59,10 +73,7 @@ public class talSpel {
 				
 				System.out.println("Credits\n");
 				System.out.println("Creator: Max Norberg\n");
-				System.out.println("-- Press '0' to go back to main menu --");
-				if (in.nextInt() == 0) {
-					return;
-				}
+				pressZero();
 				break;
 				
 			case 4:
@@ -70,17 +81,58 @@ public class talSpel {
 				
 				System.out.println("Exit\n");
 				System.out.println("Are you sure you want to exit?");
-				System.out.println("-- Press '1' to exit game, press '0' to go back to main menu --");
+				
+				success = false; 
+				
+				while (!success) {
+					
+				
+				try {
+					System.out.println("-- Press '1' to exit game, press '0' to go back to main menu --");
+					
+					again = in.nextInt(); 
+					success = true;
+				
+					if (again < 0 || again > 1) {
+						System.out.println("** You did not enter one of the given numbers. Try again. **");
+						success = false;
+					}
+					
+					
+					if (again == 0) {
+						run();
+					} else if (again == 1) {
+						System.out.println("Game is closed.");
+						System.exit(0);
+					} 
+					}
+					catch (Exception e) {
+						in.next();
+						System.out.println("** You did not enter a integer. Try again. **");
+						
+						
+					}
+					}
+				
+				
 				again = in.nextInt();
 				if (again == 1) {
 					System.out.println("Game is closed.");
 					System.exit(0);
 				} else if (again == 0) {
-					return;
+					repeat = true;
 				}
 			}
 		
-	} while (repeat == 1);
+	} 
+			catch (Exception e) {
+				in.next();
+				System.out.println("** You did not enter a integer. Try again. **");
+				
+				
+			}
+			} 
+		}
 
 
 	
@@ -89,197 +141,204 @@ public class talSpel {
 	
 	int intervall;
 	int diff;
-	double randomNumber;
+	double randomNumber; 
 	int userGuess;  
-	int iterationer;
-	int playAgain;
-
+	
+	
 	System.out.println("Game starting...\n");
 	System.out.println("Choose the highest number in the interval\n");
-	intervall = in.nextInt();
-	
-	randomNumber = (int)(Math.random() * ((intervall - 0) + 1));
-	System.out.println("\n");
-	do {
-		System.out.println("Choose a difficulty\n");
-		System.out.println("Press '1' for Easy difficulty, '2' for Medium difficulty and '3' for Hard difficulty");
-		diff = in.nextInt();
-		if (diff > 3 || diff < 0) {
-			System.out.println();
-		}
-	} while (diff > 3 || diff < 0);
-	
-	if (diff == 1) { 
-		System.out.println("Easy difficulty\n");
-		iterationer = 14;
-		for (int i = 0; i <= iterationer; i++) {
-			
-			System.out.println("Guess a number between 0 and " + intervall + "!");
-			userGuess = in.nextInt();
-			if (userGuess < randomNumber) { 
-				System.out.println("Your guess is too low!");
-			} else if (userGuess > randomNumber) {
-				System.out.println("Your guess is too high!");
-			} else if (userGuess == randomNumber) {
-				System.out.println("You guessed the right number!");
-				System.out.println("-- Press '1' play again, press '0' to exit game --");
-				playAgain = in.nextInt();
-				if (playAgain == 0) {
-					System.out.println("Game is closed.");
-					System.exit(0);
-				} else if (playAgain == 1) {
-					game();
-				}
-				
-			}
-			
-			
-			if (i == iterationer) {
-				System.out.println("The right number was " + randomNumber + "\n");
-				System.out.println("You ran out of attempts, too bad! Do you want to play again?\n");
-				System.out.println("-- Press '1' play again, press '0' to exit game --");
-				playAgain = in.nextInt();
-				if (playAgain == 0) {
-					System.out.println("Game is closed.");
-					System.exit(0);
-				} else if (playAgain == 1) {
-					game();
-				}
-			}
-		}
+		intervall = in.nextInt();
 		
-	} else if (diff == 2) {
-		System.out.println("Medium difficulty\n");
-		iterationer = 9;
-		for (int i = 0; i <= iterationer; i++) {
-			
-			System.out.println("Guess a number between 0 and " + intervall + "!");
-			userGuess = in.nextInt();
-			if (userGuess < randomNumber) { 
-				System.out.println("Your guess is too low!");
-			} else if (userGuess > randomNumber) {
-				System.out.println("Your guess is too high!");
-			} else if (userGuess == randomNumber) {
-				System.out.println("You guessed the right number!");
-				System.out.println("-- Press '1' play again, press '0' to exit game --");
-				playAgain = in.nextInt();
-				if (playAgain == 0) {
-					System.out.println("Game is closed.");
-					System.exit(0);
-				} else if (playAgain == 1) {
-					game();
-				}
-			}
-			
-			if (i == iterationer) {
-				System.out.println("The right number was " + randomNumber + "\n");
-				System.out.println("You ran out of attempts, too bad! Do you want to play again?\n");
-				System.out.println("-- Press '1' play again, press '0' to exit game --");
-				playAgain = in.nextInt();
-				if (playAgain == 0) {
-					System.out.println("Game is closed.");
-					System.exit(0);
-				} else if (playAgain == 1) {
-					game();
-				}
-			}
-			
-			
-		}
 		
-	} else if (diff == 3) {
-		System.out.println("Hard difficulty\n");
-		iterationer = 4;
-		for (int i = 0; i <= iterationer; i++) {
+		randomNumber = (int)(Math.random() * ((intervall - 0) + 1));
+		System.out.println("\n");
+	
+			System.out.println("Choose a difficulty\n");
+			System.out.println("Press '1' for Easy difficulty, '2' for Medium difficulty and '3' for Hard difficulty");
 			
-			System.out.println("Guess a number between 0 and " + intervall + "!");
-			userGuess = in.nextInt();
-			if (userGuess < randomNumber) { 
-				System.out.println("Your guess is too low!");
-			} else if (userGuess > randomNumber) {
-				System.out.println("Your guess is too high!");
-			} else if (userGuess == randomNumber) {
-				System.out.println("You guessed the right number!");
-				System.out.println("-- Press '1' play again, press '0' to exit game --");
-				playAgain = in.nextInt();
-				if (playAgain == 0) {
-					System.out.println("Game is closed.");
-					System.exit(0);
-				} else if (playAgain == 1) {
-					game();
-				}
-			}
 			
-			if (i == iterationer) {
-				System.out.println("The right number was " + randomNumber + "\n");
-				System.out.println("You ran out of attempts, too bad! Do you want to play again?\n");
-				System.out.println("-- Press '1' play again, press '0' to exit game --");
-				
-				
-				boolean success = false;
-				
-				while (!success) {
-					
-				
-				try {
-				playAgain = in.nextInt();
-				success = true;
-				if (playAgain == 0) {
-					System.out.println("Game is closed.");
-					System.exit(0);
-				} else if (playAgain == 1) {
-					game();
-				}
-				}
-				catch (Exception e) {
-					in.next();
-					System.out.println("** Please write an integer **");
+			
+			
+			do {
+				diff = in.nextInt();
+				if (diff > 3 || diff < 0) {
+					System.out.println("** You did not enter a integer. Try again. **");
 					
 				}
+			
+			
+			} while (diff > 3 || diff < 0); 
+			 
+			
+		
+		if (diff == 1) { 
+			System.out.println("Easy difficulty\n");
+			for (int i = 14; i >= 0; i--) { 
+				
+				System.out.println("Guess a number between 0 and " + intervall + "!");
+				userGuess = in.nextInt();
+				if (userGuess < randomNumber) { 
+					System.out.println("Your guess is too low!");
+				} else if (userGuess > randomNumber) {
+					System.out.println("Your guess is too high!");
+				} else if (userGuess == randomNumber) {
+					System.out.println("You guessed the right number!");
+					playAgain();
+					
 				}
 				
 				
+				
+				if (i == 0) { 
+					System.out.println("The right number was " + randomNumber + "\n");
+					System.out.println("You ran out of attempts, too bad! Do you want to play again?\n");
+					playAgain();
+				}
+				System.out.println("You have " + i + " guesses left...\n");
 			}
-		}
-		
-	}
-} public static void tryCatch() {
-		Scanner input = new Scanner(System.in);
-		int[] numbers = new int[2];
-		int index = 0;
-		boolean success = false;
-	
-		for (int i = 0; i < numbers.length; i++) { // fyller arrayen med tal 1..10
-	
-			numbers[i] = i;  
-		}
-	 
-		while (!success) { // loopar arrayen tills att användaren skrivit in en int
-	
-			try {
-				System.out.print("-- Press '0' to go back to main menu --");
-				index = input.nextInt(); 
-				System.out.println();
-				success = true; // om rätt datatyp som input avslutas while-loopen
-	
-			} catch (Exception e) { // fixar felaktig input
-				input.next(); // behövs för att göra sig av med felaktig input ovan
-				System.out.println("** Please write '0' or '1' **"); // eget felmeddelande till användare 
-				System.out.println(e); // debugging för programmeraren
+			
+		} else if (diff == 2) {
+			System.out.println("Medium difficulty\n");
+			
+			for (int i = 9; i >= 0; i--) {
+				
+				System.out.println("Guess a number between 0 and " + intervall + "!");
+				userGuess = in.nextInt();
+				if (userGuess < randomNumber) { 
+					System.out.println("Your guess is too low!");
+				} else if (userGuess > randomNumber) {
+					System.out.println("Your guess is too high!");
+				} else if (userGuess == randomNumber) {
+					System.out.println("You guessed the right number!");
+					playAgain();
+					
+				}
+				
+				if (i == 0) {
+					System.out.println("The right number was " + randomNumber + "\n");
+					System.out.println("You ran out of attempts, too bad! Do you want to play again?\n");
+					playAgain();
+					
+				}
+				System.out.println("You have " + i + " guesses left...\n");
+				
+			}
+			
+		} else if (diff == 3) {
+			System.out.println("Hard difficulty\n");
+			
+			for (int i = 4; i >= 0; i--) {
+				
+				System.out.println("Guess a number between 0 and " + intervall + "!");
+				userGuess = in.nextInt();
+				if (userGuess < randomNumber) { 
+					System.out.println("Your guess is too low!");
+				} else if (userGuess > randomNumber) {
+					System.out.println("Your guess is too high!");
+				} else if (userGuess == randomNumber) {
+					System.out.println("You guessed the right number!");
+					playAgain();
+					
+				}
+				
+				if (i == 0) {
+					System.out.println("The right number was " + randomNumber + "\n");
+					System.out.println("You ran out of attempts, too bad! Do you want to play again?\n");
+					playAgain();
+					}
+				System.out.println("You have " + i + " guesses left...\n");
 			}
 			
 		}
+		 
+			
 	
+	
+} public static void playAgain() {
+	
+	boolean success = false;
+	int playAgain;
+	
+	Scanner in = new Scanner(System.in);
+	
+	
+	
+	while (!success) {
+		
+		
 		try {
-			System.out.println();
-	
-		} catch (Exception e) { // om input tidigare större än 10 hamnar index out of bounds
-			System.out.println("** Please write '0' or '1' **"); 
-			System.out.println(e);
+		System.out.println("-- Press '1' play again, press '2' to go back to main menu, or press '0' to exit game --");
+		
+		playAgain = in.nextInt();
+		success = true;
+		
+		if (playAgain < 0 || playAgain > 2) {
+			System.out.println("** You did not enter one of the given numbers. Try again. **");
+			success = false;
 		}
+		
+		
+		if (playAgain == 0) {
+			System.out.println("Game is closed.");
+			System.exit(0);
+		} else if (playAgain == 1) {
+			game();
+		} else if (playAgain == 2) {
+			run();
+		}
+		}
+		catch (Exception e) {
+			in.next();
+			System.out.println("** You did not enter a integer. Try again. **");
+			
+			
+		}
+		}
+	
+		
+	in.close();
+
+
+} 
+/**
+ * @param
+ */
+
+public static void pressZero() {
+		boolean success = false;
+		int goBack;
+		
+		Scanner in = new Scanner(System.in);
+		
+		
+		
+		while (!success) {
+			
+			
+			try {
+			System.out.println("-- Press '0' to go back to main menu --");
+			
+			goBack = in.nextInt();
+			success = true;
+			
+			if (goBack < 0 || goBack > 0) {
+				System.out.println("** You did not enter the number '0'. Try again. **");
+				success = false;
+			}
+			
+			
+			if (goBack == 0) {
+				run(); 
+			}
+			} catch (Exception e) {
+				in.next();
+				System.out.println("** You did not enter a integer. Try again. **");
+				
+				
+			}
+			}
+			in.close(); 
+
 
 }
-
-
 }
-
